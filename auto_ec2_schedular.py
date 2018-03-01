b9c4dafac36e4d279822fadf8216e728f9effdf0
aranga-nana <aranga.nanayakkara@gmail.com>
2018-03-01 16:27:22 +1100

    stat and stop done

--- auto_ec2_schedular.py
+++ auto_ec2_schedular.py
@@ -17,19 +17,25 @@ cm= datetime_with_tz.minute
 
 d={"1":"name"}
 
-def findstop(tags):
+def start_end_time(arg,tags):
    v =""
+   r[0]=11121212
+   r[1]=2323323
    for x in tags:
-	if x['Key'] == 'time:stop':
+	if x['Key'] == arg:
 	   v = x['Value']
-   return v
-def stopInstance(ec2,iid):
-    print('stopiing instance ',iid)
-    print ec2.stop_instances(InstanceIds=[iid])   	
+           timepart= v.split(':')
+           r[0]= int(timepart[0])
+           r[1]=int(timepart[1])
 
-def suspendAsg(name):
-    client = boto3.client('autoscaling',region_name='ap-southeast-2') 
-    print client.suspend_processes(AutoScalingGroupName=name)   
+   return r
+
+def stopInstance(ec2,iid):
+    #print('stopiing instance ',iid)
+    print ec2.stop_instances(InstanceIds=[iid]) 
+  	
+def startInstances(ec2,iid):
+    print ec2.start_instances(InstanceIds=[iid])
 
 
 def initaliseall():
@@ -43,10 +49,9 @@ def initaliseall():
         for instance in asg['Instances']:
             iid= instance['InstanceId']
             d[iid] = asg['AutoScalingGroupName']
+
             
-            #d[iid]= asg['AutoScalingGroupName']
 initaliseall()
-print d
 
 print '============================================='
 ec2 = boto3.client('ec2',region_name='ap-southeast-2')
@@ -60,28 +65,58 @@ for r in reservations['Reservations']:
         print('checking instance id',iid)
         date= i.get('LaunchTime')
         tags =i['Tags']
-        print tags
-        strTime = findstop(tags)
-	timepart= strTime.split(':')
-        print "current",ch,";",cm
-        h= int(timepart[0])
-	m= int(timepart[1]) 
-        print "instance",h,";",m
-        print cm > m
+        #print tags
+        ## stop
+        r = start_end_time('time:stop',tags)
+        print "instance",r[0],";",r[1]
         stated = i['State']
         stateId = stated.get('Code') 
         print stateId
+        executeStop = False
         if stateId == 16 or stateId == 0:
-	   if ch > h:
-              stopInstance(iid)
-           if h==ch and cm >= m:
+	   if ch > r[0]:
+              if iid in d:
+                 suspendAsg(d[iid])
+              time.sleep(0.300)
+              stopInstance(ec2,iid)
+              executeStop = True
+           if ch == r[0] and cm >= r[1]:
               print "about to stop",iid
               if iid in d:
                  suspendAsg(d[iid]) 
               time.sleep(0.300)               
-              stopInstance(ec2,iid) 
+              stopInstance(ec2,iid)
+              executeStop = True 
+        ## start
+          
+        if not executeStop:
+           r = start_end_time('time:start',tags)
+           print "instance",r[0],";",r[1]
+           if stateId == 80 or stateId == 64:
+              if ch > r[0]:
+                 startInstances(ec2,iid)                 
+              if ch == r[0] and cm >= r[1]:
+                 startInstances(ec2,iid)
 
+''' 
+        stated = i['State']
+        stateId = stated.get('Code')
+        print stateId
+        if stateId == 16 or stateId == 0:
+           if ch > r[0]:
+              if iid in d:
+                 print()
+                 #suspendAsg(d[iid])
+              #startInstance(iid)
+           if ch == r[0] and cm >= r[1]:
+              print "about to stop",iid
+              if iid in d:
+                 #suspendAsg(d[iid])
+                 print() 
+              time.sleep(0.300)
+              #stopInstance(ec2,iid)
 
+'''
 #autoscale = boto3.client('autoscaling',region_name='ap-southeast-2')
 #response = autoscale.describe_auto_scaling_groups()
 #name=response['AutoScalingGroups'][0]['AutoScalingGroupName']
